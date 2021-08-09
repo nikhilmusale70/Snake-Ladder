1b947b1507468e381fd3853dd59cc773244a04e6
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 23:29:04 +0530

    [Nikhil] - Done with uc2

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,7 +1,12 @@
+import java.util.Random;
+
 public class Snake_Ladder {
     public static void main(String[] args) {
+        Random ran=new Random();
         int position=0;
 
-        System.out.println("Player 1 at position :- " + position);
+        int die_no = ran.nextInt(6)+1;
+
+        System.out.println("die's result :- " + die_no);
     }
 }
