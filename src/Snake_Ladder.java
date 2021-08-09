9d69a5865bfda73f9c1ab0b1a0938e67d186bf6c
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 23:24:15 +0530

    [Nikhil] - Done with uc1

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,5 +1,7 @@
 public class Snake_Ladder {
     public static void main(String[] args) {
-        
+        int position=0;
+
+        System.out.println("Player 1 at position :- " + position);
     }
 }
