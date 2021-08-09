5cf8e6fae07c926c9fb33380ee678f72bee216dc
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 23:40:13 +0530

    [Nikhil] - added uc3

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,12 +1,31 @@
 import java.util.Random;
 
 public class Snake_Ladder {
+
+    public static int no_play=0;
+    public static int ladder=1;
+    public static int snake=2;
+
     public static void main(String[] args) {
         Random ran=new Random();
         int position=0;
-
+        int option=ran.nextInt(3);
         int die_no = ran.nextInt(6)+1;
 
-        System.out.println("die's result :- " + die_no);
+        System.out.println("you got "+ die_no);
+
+        if(option==ladder){
+            System.out.println("ladder");
+            position=die_no+position;
+        }
+        else if (option==snake){
+            System.out.println("snake");
+            position=position-die_no;
+        }
+        else{
+            System.out.println("No-Play");
+        }
+
+        System.out.println("Player 1 Currently on :- " + position);
     }
 }
