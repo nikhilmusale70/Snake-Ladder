d4b6a1d7bc154eca941d240ffa26d6a2824e2bc3
Nikhil <Nikhilmusale70@gmail.com>
2021-08-09 23:59:52 +0530

    [Nikhil] - added uc4

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -9,23 +9,26 @@ public class Snake_Ladder {
     public static void main(String[] args) {
         Random ran=new Random();
         int position=0;
-        int option=ran.nextInt(3);
-        int die_no = ran.nextInt(6)+1;
 
-        System.out.println("you got "+ die_no);
+        do {
+            int option = ran.nextInt(3);
+            int die_no = ran.nextInt(6) + 1;
 
-        if(option==ladder){
-            System.out.println("ladder");
-            position=die_no+position;
-        }
-        else if (option==snake){
-            System.out.println("snake");
-            position=position-die_no;
-        }
-        else{
-            System.out.println("No-Play");
-        }
+            System.out.println("you got " + die_no);
 
-        System.out.println("Player 1 Currently on :- " + position);
+            if (option == ladder) {
+                System.out.println("ladder");
+                position = die_no + position;
+            } else if (option == snake) {
+                System.out.println("snake");
+                position = position - die_no;
+                if (position < 0)
+                    position = 0;
+            } else {
+                System.out.println("No-Play");
+            }
+
+            System.out.println("Player 1 Currently on :- " + position);
+        }while (position<100||position>100);
     }
 }
