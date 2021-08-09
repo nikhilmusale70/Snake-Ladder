0fb724143b2ce51908f404a671acaf302448a00f
Nikhil <Nikhilmusale70@gmail.com>
2021-08-10 00:32:37 +0530

    [Nikhil] - Added uc7

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -8,32 +8,62 @@ public class Snake_Ladder {
 
     public static void main(String[] args) {
         Random ran=new Random();
-        int position=0;
+        int position1=0;
+        int position2=0;
         int i=0;
+        int j=0;
 
         do {
+            //for player 1
             int option = ran.nextInt(3);
             int die_no = ran.nextInt(6) + 1;
             i++;
 
-            System.out.println("you got " + die_no);
+            //System.out.println("you got " + die_no);
 
             if (option == ladder) {
-                System.out.println("ladder");
-                position = die_no + position;
-                if (position>100)
-                    position=position-die_no;
+                //System.out.println("ladder");
+                position1 = die_no + position1;
+                if (position1>100)
+                    position1=position1-die_no;
             } else if (option == snake) {
-                System.out.println("snake");
-                position = position - die_no;
-                if (position < 0)
-                    position = 0;
+                //System.out.println("snake");
+                position1 = position1 - die_no;
+                if (position1 < 0)
+                    position1 = 0;
             } else {
-                System.out.println("No-Play");
+                //System.out.println("No-Play");
             }
 
-            System.out.println("Player 1 Currently on :- " + position);
-        }while (position<100);
-        System.out.println("die rolled for :- "+i+" times");
+            //System.out.println("Player 1 Currently on :- " + position1);
+
+       //for player 2
+
+            int option2 = ran.nextInt(3);
+            int die_no2 = ran.nextInt(6) + 1;
+            j++;
+
+            //System.out.println("you got " + die_no);
+
+            if (option2 == ladder) {
+                //System.out.println("ladder");
+                position2 = die_no2 + position2;
+                if (position2>100)
+                    position2=position2-die_no2;
+            } else if (option == snake) {
+                //System.out.println("snake");
+                position2 = position2 - die_no2;
+                if (position2 < 0)
+                    position2 = 0;
+            } else {
+                //System.out.println("No-Play");
+            }
+
+        }while (position1<100 && position2<100);
+        if (position1==100)
+            System.out.println("player1 won in "+i+" die thrown");
+        if (position2==100)
+            System.out.println("Player2 won in "+j+" die thrown");
+        System.out.println(position1);
     }
 }
